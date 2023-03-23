298c1b4e1305f015e0150b14841802a377f0df1f
Lucía Sánchez <lus4ney@gmail.com>
2023-03-23 20:52:18 +0100

    Modificación en rama develop. Lucía Sánchez

--- src/HOLAMUNDO.java
+++ src/HOLAMUNDO.java
@@ -3,7 +3,10 @@ public class HOLAMUNDO {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.print("Hola Mundo.");
+System.out.println("Hola Mundo.");
+System.out.println("Hola Mundo.");
+System.out.println("Hola Mundo.");
+System.out.println("Hola Mundo.");
 	}
 
 }
